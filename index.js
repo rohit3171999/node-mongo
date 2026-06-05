0add4ee8c9250af7b098605b7e6f63b035671018
Rohit Sharma <rohit3171999@gmail.com>
2026-06-05 11:11:24 +0530

    Add MongoDB connection setup using Mongoose

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const mongoose = require('mongoose'); // Just to connect to MongoDB
+
+mongoose.connect(
+    "mongodb://127.0.0.1:27017/rohitdb"
+)
+.then(() =>{
+    console.log("Mongo Connected");
+})
+.catch((err) => {
+    console.log("err");
+});
+
 const express = require('express');
 
 const app = express();
