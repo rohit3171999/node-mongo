1d2a21a36a4e50d5e66901c1781a796b024b5aae
Rohit Sharma <rohit3171999@gmail.com>
2026-06-08 17:50:18 +0530

    Update MongoDB connection URI to use host.docker.internal

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Product = require("./models/Product");
 const Order = require("./models/Order");
 
 mongoose.connect(
-    "mongodb://127.0.0.1:27017/rohitdb"
+    "mongodb://host.docker.internal:27017/rohitdb"
 )
     .then(() => {
         console.log("Mongo Connected");
